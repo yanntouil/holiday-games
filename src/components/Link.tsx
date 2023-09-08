d8cb785c717709b389df6b70cc5c4ba0749a03c3
Yann Touil <yanntouil@gmail.com>
2023-09-08 22:21:41 -0100

    update

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react"
 import { Link as WouterLink, type LinkProps as WouterLinkProps } from "wouter"
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
